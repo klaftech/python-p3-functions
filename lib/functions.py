84c1f9c168d6216fa84e515055efcd19fac07612
Elchonon <ekn400@gmail.com>
2024-12-09 09:52:53 -0500

    lab

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,25 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     pass
+    return num1 + num2
 
 def halve(number):
     pass
+    if isinstance(number, (float,int)):
+        return number / 2
+    else:
+        return None
+
+print("Hello, programmer!")
